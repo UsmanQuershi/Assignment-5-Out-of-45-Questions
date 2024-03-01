83b6a1ececcbe67900142b28c2be083e5a945da2
Usman <usmanofficial943@gmail.com>
2024-03-01 11:20:50 +0500

    5 Completed

---
+++ main.js
@@ -0,0 +1,15 @@
+//Q1 Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var person_name = "Eric";
+console.log("Hello" ,person_name, "would you like to learn some Python today?");
+
+let per_name = "osman";
+console.log(per_name.toUpperCase())
+console.log(per_name.toLowerCase())
+console.log(per_name.charAt(0).toUpperCase())
+
+
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new."');
+
+const quote = 'Albert Einstein once said, “A person who never made a mistake never tried anything new."';
+
+console.log(quote);
---
+++ main.ts
@@ -0,0 +1,23 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let person_name = "Eric";
+console.log("Hello" ,person_name, "would you like to learn some Python today?")
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let per_name = "Osman";
+console.log(per_name.toUpperCase())
+console.log(per_name.toLowerCase())
+console.log(per_name.charAt(0).toUpperCase())
+
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new."');
+
+const quote = 'Albert Einstein once said, “A person who never made a mistake never tried anything new."';
+
+console.log(quote);
+
+
+
+
+
+
